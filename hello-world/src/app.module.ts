06afb9f4754c1e5f46216f61d6e32e21fc3cca6c
Valeriya <shinvaleriya1808@gmail.com>
2025-02-03 14:27:41 +0100

    Creating hello World-> Module, Service, Controller

--- hello-world/src/app.module.ts
+++ hello-world/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ModuleModule } from './module/module.module';
+import { ServiceService } from './service/service.service';
+import { ControllerController } from './controller/controller.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ModuleModule],
+  controllers: [AppController, ControllerController],
+  providers: [AppService, ServiceService],
 })
 export class AppModule {}
